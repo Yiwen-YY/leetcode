eef26dfc183914474963eccb57e26d6a72c32714
Yiwen <yang.w.yyw@gmail.com>
2023-12-15 00:02:16 +0800

    :sparkles: Add 0704

--- src/com/array/_0704/BinarySearch.java
+++
@@ -1,4 +0,0 @@
-package com.array._0704;
-
-public class BinarySearch {
-}
---
+++ src/com/array/_0704/Solution.java
@@ -0,0 +1,31 @@
+package com.array._0704;
+// Binary Search
+public class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        if (target < nums[left] || target > nums[right]){
+            return -1;
+        }
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else{
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        System.out.println(solution.search(nums, target));
+    }
+}
