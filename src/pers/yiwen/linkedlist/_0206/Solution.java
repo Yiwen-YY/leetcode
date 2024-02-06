3a80e62db05eb7bec18ab1ecaf2f07018a768072
Yiwen <yang.w.yyw@gmail.com>
2024-02-06 23:46:16 +0800

    :sparkles: Add 0206

---
+++ src/pers/yiwen/linkedlist/_0206/Solution.java
@@ -0,0 +1,36 @@
+package pers.yiwen.linkedlist._0206;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode prev = null;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5};
+        ListNode head = new ListNode(nums[0]);
+        ListNode node = head;
+
+        for (int i = 1; i < nums.length; i++) {
+            node.next = new ListNode(nums[i]);
+            node = node.next;
+        }
+
+        Solution solution = new Solution();
+        node = solution.reverseList(head);
+
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+    }
+}
