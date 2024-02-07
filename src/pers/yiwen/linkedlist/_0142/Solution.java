9ebb3c07c780f0f6a909e0fce458d45bad32730c
Yiwen <yang.w.yyw@gmail.com>
2024-02-07 23:31:41 +0000

    :sparkles: Add 0142

---
+++ src/pers/yiwen/linkedlist/_0142/Solution.java
@@ -0,0 +1,27 @@
+package pers.yiwen.linkedlist._0142;
+
+import pers.yiwen.linkedlist.ListNode;
+
+// Linked List Cycle II
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow) {
+                slow = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
+
+        return null;
+    }
+}
