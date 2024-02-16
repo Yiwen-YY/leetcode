ec53b1bcb0e2e66d3ba6ac25644fb9fe232daae6
Yiwen <yang.w.yyw@gmail.com>
2024-02-16 23:35:10 +0000

    :bug: fix 0151

--- src/pers/yiwen/string/_0151/Solution.java
+++ src/pers/yiwen/string/_0151/Solution.java
@@ -4,6 +4,21 @@ public class Solution {
     public String reverseWords(String s) {
         char[] str = s.toCharArray();
 
-        
+        int index = 0;
+        while (str[index] == ' ') {
+            index++;
+        }
+        str = removeSpace(str, 0, index);
+
+        return s;
+    }
+
+    public char[] removeSpace(char[] str, int pre, int behind) {
+        while (behind < str.length){
+            str[pre] = str[behind + 1];
+            pre++;
+            behind++;
+        }
+        return str;
     }
 }
