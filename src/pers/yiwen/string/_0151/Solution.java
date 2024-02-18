37dbe94c08d6a815aaea0f279e3b859ec4c68198
Yiwen <yang.w.yyw@gmail.com>
2024-02-18 23:48:38 +0000

    :bug: fix 0151

--- src/pers/yiwen/string/_0151/Solution.java
+++ src/pers/yiwen/string/_0151/Solution.java
@@ -1,24 +1,57 @@
 package pers.yiwen.string._0151;
+
 // Reverse Words in a String
 public class Solution {
     public String reverseWords(String s) {
         char[] str = s.toCharArray();
 
-        int index = 0;
-        while (str[index] == ' ') {
-            index++;
-        }
-        str = removeSpace(str, 0, index);
+        reverse(str, 0, str.length - 1);
+        reverseWords(str, str.length);
 
-        return s;
+        return cleanSpaces(str, str.length);
     }
 
-    public char[] removeSpace(char[] str, int pre, int behind) {
-        while (behind < str.length){
-            str[pre] = str[behind + 1];
-            pre++;
-            behind++;
+    private void reverseWords(char[]str, int len) {
+        int left = 0, right = 0;
+
+        while (left < len) {
+            while (left < right || left < len && str[left] == ' ') {
+                left++;
+            }
+            while (right < left || right < len && str[right] != ' ') {
+                right++;
+            }
+            reverse(str, left, right - 1);
         }
-        return str;
+    }
+
+    private void reverse(char[] str, int left, int right) {
+        while (left < right) {
+            char temp = str[left];
+            str[left] = str[right];
+            str[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    String cleanSpaces(char[] a, int n) {
+        int i = 0, j = 0;
+
+        while (j < n) {
+            while (j < n && a[j] == ' ') j++;             // skip spaces
+            while (j < n && a[j] != ' ') a[i++] = a[j++]; // keep non spaces
+            while (j < n && a[j] == ' ') j++;             // skip spaces
+            if (j < n) a[i++] = ' ';                      // keep only one space
+        }
+
+        return new String(a).substring(0, i);
+    }
+    public static void main(String[] args) {
+        String s = "  hello world  ";
+
+        Solution solution = new Solution();
+        System.out.println(solution.reverseWords(s));
     }
 }
