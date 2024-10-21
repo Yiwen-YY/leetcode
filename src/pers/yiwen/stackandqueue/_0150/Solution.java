8f38250cbec8f8071e3d5bf10ea6dbe4e9c20309
Yiwen <yang.w.yyw@gmail.com>
2024-10-21 23:19:32 +0100

    :sparkles: Add 0150

--- src/pers/yiwen/stackandqueue/_0150/Solution.java
+++ src/pers/yiwen/stackandqueue/_0150/Solution.java
@@ -1,7 +1,37 @@
 package pers.yiwen.stackandqueue._0150;
+
+import java.util.Objects;
+import java.util.Stack;
+
 // Evaluate Reverse Polish Notation
 public class Solution {
     public int evalRPN(String[] tokens) {
-        
+        Stack<Integer> st = new Stack<>();
+
+        int num1, num2;
+        for (String s : tokens) {
+            if (Objects.equals(s, "+")) {
+                num2 = st.pop();
+                num1 = st.pop();
+                st.push(num1 + num2);
+            } else if (Objects.equals(s, "-")) {
+                num2 = st.pop();
+                num1 = st.pop();
+                st.push(num1 - num2);
+            } else if (Objects.equals(s, "*")) {
+                num2 = st.pop();
+                num1 = st.pop();
+                st.push(num1 * num2);
+            } else if (Objects.equals(s, "/")) {
+                num2 = st.pop();
+                num1 = st.pop();
+                st.push(num1 / num2);
+            } else {
+                st.push(Integer.valueOf(s));
+            }
+
+        }
+
+        return st.pop();
     }
 }
